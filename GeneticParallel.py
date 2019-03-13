de5e50a5d4ac43596456fba1cf4ec8b797207de3
aditya-biswas <aditya.biswas@yale.edu>
2019-03-13 16:29:20 -0400

    Create GeneticParallel.py

---
+++ GeneticParallel.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 11 15:15:20 2019
+
+@author: adityabiswas
+"""
+
+
+import numpy as np
+import multiprocessing
+import pickle
+import os
+
+
+class GeneticFeatureSelection(object):
+    def __init__(self, fitnessFunc, X, otherData, varNames, numFeats = 10, popSize = 100, 
+                             numClones = 1, mutationRate = 0.01, n_cores = 1, population = None,
+                             verbose = True, folder = None):
+        
+        self.fitnessFunc = fitnessFunc
+        self.X = X
+        self.otherData = otherData if type(otherData) is tuple else (otherData,)
+        self.varNames = varNames
+        self.numFeats = numFeats
+        self.popSize = popSize
+        self.numClones = numClones
+        self.mutationRate = mutationRate
+        self.generation = 0
+        self.n_cores = n_cores
+        self.verbose = verbose
+        self.folder = folder
+        
+        ## init population with random feature sets, possibly appending
+        ## to a previous population till popSize is met
+        randomPopulation = np.array([np.random.choice(np.arange(len(self.varNames)), 
+                                    self.numFeats, replace = False) for i in range(popSize)])
+        if population is None:
+            self.population = randomPopulation
+        else:
+            if type(population) is str:
+                folderErrMsg = "Folder must be provided if population parameter is a string"
+                assert folder is not None, folderErrMsg
+                population = self.load(population)
+            
+            popErrMsg = "Provided population contains the wrong number of features"
+            assert type(population) is np.ndarray and np.shape(population)[1] == numFeats, popErrMsg
+            if len(population) >= popSize:
+                self.population = population[:popSize,:]
+            else:
+                k = popSize - len(population)
+                self.population = np.concatenate([population, randomPopulation[:k,:]], axis = 0)
+        
+        self.clearScores()
+        self.probs = self.linearPMF(popSize)
+        self.open()
+        
+    def recombine(self, x0, x1):
+        x0, x1 = set(x0), set(x1)
+        z = x0 & x1     # find common features
+        choices = np.array(list((x0 | x1) - z))     # combine uncommon features
+        z = np.array(list(z))
+        np.random.shuffle(choices)
+        k = self.numFeats - len(z)
+        y = np.concatenate((choices[:k], z)) # randomly pick k uncommon features
+        return y
+    
+    def mutate(self, individual):
+        # randomly chooses whether to mutate each var
+        mutateVar = np.random.uniform(size = self.numFeats) < self.mutationRate
+        for i in range(self.numFeats):
+            if mutateVar[i]: # if chosen, replace with var not currently in genotype
+                choices = np.array(list(set(np.arange(len(self.varNames))) - set(individual)))
+                individual[i] = np.random.choice(choices)
+        return individual
+    
+        
+    def mate(self, population):
+        # samples all mating pairs
+        size = self.popSize - self.numClones
+        father = np.random.choice(np.arange(self.popSize),
+                                    size = size, p = self.probs)
+        mother = np.random.choice(np.arange(self.popSize),
+                                    size = size, p = self.probs)
+        
+        # genetic recombination and mutation for all pairs
+        children = np.empty((size, self.numFeats), dtype = np.int32)
+        for i in range(size):
+            child = self.recombine(population[father[i]], 
+                                    population[mother[i]])
+            children[i,:] = self.mutate(child)
+        return children
+
+    def newGeneration(self):
+        populationA = self.population[:self.numClones] # clone copies
+        populationB = self.mate(self.population) # generate mating offspring
+        self.population = np.concatenate((populationA, populationB), axis = 0)
+        assert len(self.population) == self.popSize
+        
+    def evaluate(self):
+        self.clearScores()
+        assert self.pool is not None
+        # train models on each member of the population's training set
+        # and evaluate on the test set
+        generator = ((i, self.X[:,self.population[i]],
+                      *self.otherData) for i in range(self.popSize))
+        for i, score in self.pool.map(self.fitnessFunc, generator, chunksize = 1):
+            self.scores[i] = score
+        
+        # sort the population and fitness scores by the performance
+        sorter = np.argsort(self.scores)[::-1]
+        self.scores = self.scores[sorter]
+        self.population = self.population[sorter]
+            
+    def evolve(self):
+        self.newGeneration() # make new gen using last gen fitness scores
+        self.evaluate() # generate current fitness scores and sort
+        if self.verbose:
+            print('Fitness calculations finished for generation: ', self.generation)
+            print('Max: ', np.round(np.max(self.scores), 4), 
+                  '   |   Min: ', np.round(np.min(self.scores), 4))
+        self.generation += 1
+    
+    def getBest(self):
+        # returns the best fitness score in current population 
+        # and its associated feature set
+        m = np.max(self.scores)
+        idx = np.where(self.scores == m)[0][0]
+        return (m, self.varNames[self.population[idx]])
+    
+    
+    def linearPMF(self, N):
+        # rank-based linearly decreasing pmf (top ranked is x=0)
+        return -(2*np.arange(N) - 2*N + 1)/(N**2)
+
+    
+    def getPopulation(self):
+        return self.population
+    def getScores(self):
+        return self.scores        
+    def getGeneration(self):
+        return self.generation
+    def setMutationRate(self, rate):
+        self.mutationRate = rate
+    def setFolder(self, folder):
+        self.folder = folder
+    def clearScores(self):
+        self.scores = np.empty(self.popSize)
+    
+    def open(self):
+        self.pool = multiprocessing.Pool(self.n_cores)     
+    def close(self):
+        self.pool.close()
+        
+    def save(self, name):
+        name += '.pickle'
+        with open(os.path.join(self.folder, name), 'wb') as output:
+            pickle.dump(self.population, output, -1)
+    
+    def load(self, name):
+        name += '.pickle'
+        with open(os.path.join(self.folder, name), 'rb') as input_:
+            return pickle.load(input_)        
+        
